a6b5b6b3de2bface22c4994896fee6323bd7176e
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2022-08-18 09:57:41 -0300

    chore: adding style sheet

--- exercicios/src/components/Multi.tsx
+++ exercicios/src/components/Multi.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Text } from "react-native";
+import Estilo from './estilo'
 
-export default function Comp() {
-	return <Text>Comp #Oficial</Text>
+function Comp() {
+	return <Text style={Estilo.fontG}>Comp #Oficial</Text>
 }
 
-export function Comp1() {
-	return <Text>Comp #01</Text>
+function Comp1() {
+	return <Text style={Estilo.fontG}>Comp #Oficial</Text>
 }
 
-export function Comp2() {
-	return <Text>Comp #02</Text>
+function Comp2() {
+	return <Text style={Estilo.fontG}>Comp #Oficial</Text>
 }
+
+export { Comp1, Comp2 }
+export default Comp
--- exercicios/src/components/Primeiro.tsx
+++ exercicios/src/components/Primeiro.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
+import Estilo from './estilo'
 
-export default () => {
-	return (
-		<Text>Primeiro</Text>
-	)
-}
+
+export default () => <Text style={Estilo.fontG}>Primeiro</Text>
