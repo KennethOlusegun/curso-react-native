75de965b49b4648aa09a3fe6829a3a0b1dec52de
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2022-08-18 09:36:42 -0300

    Adding first component and multiple components

---
+++ exercicios/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+import Primeiro from "./components/Primeiro";
+
+export default () => {
+	return (
+		<View>
+			<CompPadrao />
+			<Comp1 />
+			<Comp2 />
+			<Primeiro />
+		</View>
+	)
+} 
