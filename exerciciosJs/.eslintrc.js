501879c5bda122be54ba30967650385e3de824ce
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2022-09-01 08:31:10 -0300

    chore:  adding Counting component and snippet from vscode

--- exerciciosJs/.eslintrc.js
+++ exerciciosJs/.eslintrc.js
@@ -1,4 +1,29 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+{
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+        "jest": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:prettier/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [],
+    "rules": {
+        "no-unused-vars": [
+            "error",
+            {
+                "argsIgnorePattern": "^_",
+                "varsIgnorePattern": "^_"
+            }
+        ]
+    }
+}
--- exerciciosJs/src/App.js
+++ exerciciosJs/src/App.js
@@ -1,29 +1,36 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
 
-import Aleatorio from "./components/Aleatorio";
-// import MinMax from "./components/MinMax";
-// import CompPadrao, { Comp1, Comp2 } from "./components/Multi"
-// import Primeiro from "./components/Primeiro";
+import Contador from './components/Contador'
+// import Botao from './components/Botao'
+// import Titulo from './components/Titulo'
+// import Aleatorio from './components/Aleatorio'
+// import MinMax from './components/MinMax'
+// import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
 
-export default () => {
-	return (
-		<View style={styles.App}>
-			<Aleatorio min={1} max={60} />
-			<Aleatorio min={1} max={60} />
-			<Aleatorio min={1} max={60} />
-			<Aleatorio min={1} max={60} />
-			<Aleatorio min={1} max={60} />
-			<Aleatorio min={1} max={60} />
-			{/*<MinMax min={3} max={20}
-			<MinMax min={3} max={20}
-			<CompPadrao />
-			<Comp1 />
-			<Comp2 />
-			<Primeiro /> */}
-		</View>
-	)
-}
+export default () => (
+	<View>
+		<Contador inicial={100} passo={13}/>
+		<Contador />
+        {/*
+		<Botao />
+        <Titulo principal="Cadastro Produto"
+            segundario="Tela de Cadastro do Produto" />
+		 <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <MinMax min={3} max={20} />
+        <MinMax min={1} max={94} />
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro /> */}
+    </View>
+)
 
 const styles = StyleSheet.create({
 	App: {
